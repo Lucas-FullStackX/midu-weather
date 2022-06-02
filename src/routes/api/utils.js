1ead5eda3a0851a7b2477218fba46c40e5ac92e0
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2022-06-02 09:27:01 -0500

    fix

--- src/routes/api/utils.js
+++ src/routes/api/utils.js
@@ -2,6 +2,6 @@ export const FETCH_OPTIONS = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-		'X-RapidAPI-Key': '71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5'
+		'X-RapidAPI-Key': '041b3a6f90msh2aeef23f06ddccfp1198c0jsn8a4c5018655b'
 	}
 };
